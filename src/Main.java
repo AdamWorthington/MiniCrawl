07b065ef2add268f0a6b9bad8363fbf609c181dd
Rohan Barman <Rohan1Barman@gmail.com>
2014-07-17 00:27:55 -0400

    GoogleScraper.pageNum -> number of pages user wants

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
 public class Main {
 	public static void main(String[] args) {
 		
-		// sample usage of GoogleScraper object. 
-		GoogleScraper gs = new GoogleScraper();	
+		// sample usage of GoogleScraper object.
+		int pageNum = 10;
+		GoogleScraper gs = new GoogleScraper(pageNum);	
 		String[] input = gs.getInput();
 		for(String link : gs.getLinks(input))
 			System.out.println(link);
